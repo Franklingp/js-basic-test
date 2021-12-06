2de291de91e0e91e6fcfd57b7978d6fbdd44eee3
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-12-05 19:07:09 -0500

    Adding some basics tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,34 @@
 const text = "Hola Mundo";
+const fruits = ["manzana", "mango", "naranja"]
 
+
+//Comprobar si esta el string
 test("La variable debe ser un string.", () => {
     expect(text).toMatch(/Mundo/);
 });
+
+//Comprobar si hay un elemento en el arreglo
+test("Verificar frutas en arreglo", () => {
+    expect(fruits).toContain("mango");
+})
+
+//Comprobar si es un booleano verdadero
+test("Tiene que ser booleano verdadero", () => {
+    expect(true).toBeTruthy()
+})
+
+//Probar numeros, si uno es mayor que otro
+test("El numero es mayor que", () => {
+    expect(10).toBeGreaterThan(9);
+})
+
+//Probar un callback
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+}
+
+test('Probar un callback', () => {
+    reverseString("hola", (str) => {
+        expect(str).toBe("aloh")
+    })
+})
